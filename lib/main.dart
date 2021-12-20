762318e58f6f4e47e63268173f24b901274b55c0
Lucas Gomes <lggomes007@gmail.com>
2021-12-20 17:49:57 -0300

    Implementing the Contact List

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank_modulo2/screens/contacts_list.dart';
+import 'package:bytebank_modulo2/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,52 +26,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 32.0,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
